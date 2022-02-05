c39888be9ab22ad7e49f46e0364ee2c4a52e4f5c
InSeongSo <goflvhxj2547@gmail.com>
2022-02-05 16:10:21 +0900

    🎆Style: 기초 테마 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import App from './presentation/app';
+import theme from './presentation/theme';
+import ResetStyle from './reset.styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <ResetStyle />
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ frontend/src/presentation/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>스타일 세팅</h1>
+    </div>
+  );
+};
+
+export default App;
