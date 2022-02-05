5bff9331162ac50da08fb5e2dd3bd3c6f4d59f83
InSeongSo <goflvhxj2547@gmail.com>
2022-02-06 00:14:59 +0900

    ✒️Feat: 전역 스타일을 위한 파일을 직관적으로 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
-import App from './presentation/app';
-import theme from './presentation/theme';
-import ResetStyle from './reset.styles';
+import App from './presenter';
+import * as Global from './index.styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={Global.theme}>
       <CssBaseline>
-        <ResetStyle />
+        <Global.ResetStyle />
         <App />
       </CssBaseline>
     </ThemeProvider>
