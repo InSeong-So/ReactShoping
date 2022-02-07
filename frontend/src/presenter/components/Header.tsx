e22cb3c5a6a5d4b3ed9e7da1a250be9b75cd26e7
InSeongSo <goflvhxj2547@gmail.com>
2022-02-07 13:06:53 +0900

    ✒️Feat: 공통 컴포넌트 Header 추가

---
+++ frontend/src/presenter/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import logo from './Logo.png';
+import { useBloc as useCartBloc } from '../../context';
+import { UpdatedCartState, CartState } from 'cart';
+import { BlocBuilder } from '../../bloc';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  toolbar: {
+    justifyContent: 'space-between',
+    maxWidth: '800',
+  },
+}));
+
+const Header: React.FC = () => {
+  const classes = useStyles();
+  const bloc = useCartBloc();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <img src={logo} width={75} alt="logo" />
+        <IconButton color="inherit">
+          <BlocBuilder
+            bloc={bloc}
+            builder={(state: CartState) => {
+              const totalItems =
+                bloc.state.kind === 'UpdatedCartState'
+                  ? (bloc.state as UpdatedCartState).totalItems
+                  : 0;
+
+              return (
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCartIcon onClick={() => bloc.openCart()} />
+                </Badge>
+              );
+            }}
+          />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
